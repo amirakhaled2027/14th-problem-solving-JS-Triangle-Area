88b3b028b2b15098971a09ff254e41124144114f
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-06-06 23:19:49 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//Write a function that takes the base and height of a triangle and return its area
+function triangleArea(base, height) {
+    var area = 0.5 * base * height;
+    return area;
+}
+console.log(triangleArea(5, 8));
